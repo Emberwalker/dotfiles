e325fea62d039699b9ecac723cafa33d2e221105
Nathaniel Emerson <nathaniel.emerson@skyscanner.net>
2023-08-01 13:59:30 +0100

    Neovim tweaks

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,19 +3,19 @@
 -- Add any additional options here
 
 local function expandEnsure(path)
-    local expanded = vim.fn.expand(path)
-    if not vim.fn.isdirectory(expanded) then
-        vim.fn.mkdir(expanded, "p")
-    end
-    return expanded
+	local expanded = vim.fn.expand(path)
+	if not vim.fn.isdirectory(expanded) then
+		vim.fn.mkdir(expanded, "p")
+	end
+	return expanded
 end
 
 local set = vim.opt
 
 -- persistent undo
 if vim.fn.has("+undofile") then
-    set.undofile = true
-    set.undodir = expandEnsure("~/.nvim/undo")
+	set.undofile = true
+	set.undodir = expandEnsure("~/.nvim/undo")
 end
 
 -- backups
@@ -26,7 +26,10 @@ set.backupdir = expandEnsure("~/.nvim/backup")
 set.directory = expandEnsure("~/.nvim/swap")
 set.swapfile = false
 
-set.modeline = false  -- modelines have security risks associated with them
-set.smartcase = true  -- case-sensitive if there's a capital letter
-set.showmatch = true  -- highlight matching pairs
-set.colorcolumn = "120"  -- mark column 120 for a sensible guide for max line length
+set.modeline = false -- modelines have security risks associated with them
+set.smartcase = true -- case-sensitive if there's a capital letter
+set.showmatch = true -- highlight matching pairs
+
+if not vim.g.vscode then
+	set.colorcolumn = "120" -- mark column 120 for a sensible guide for max line length
+end
